d8ca228d87f3bab03faf8b077f07bc9794b0eef9
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-09-24 15:55:58 +1000

    Updated

--- sensorHubCode/Sensor Node/src/main.cpp
+++ sensorHubCode/Sensor Node/src/main.cpp
@@ -5,10 +5,10 @@
 #include <SPI.h>
 
 /**** Configure the nrf24l01 CE and CS pins ****/
-RF24 radio(7, 8);
+RF24 radio(9, 10);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
- 
+
 /*
  * User Configuration: nodeID - A unique identifier for each radio. Allows addressing
  * to change dynamically with physical changes to the mesh.
@@ -17,78 +17,94 @@ RF24Mesh mesh(radio, network);
  * A unique value from 1-255 must be configured for each node.
  */
 #define nodeID 1
- 
- 
+
 uint32_t displayTimer = 0;
- 
-struct payload_t {
+
+struct payload_t
+{
   unsigned long ms;
   unsigned long counter;
 };
- 
-void setup() {
- 
+
+void setup()
+{
+
   Serial.begin(115200);
-  while (!Serial) {
+  while (!Serial)
+  {
     // some boards need this because of native USB capability
   }
- 
+
   // Set the nodeID manually
   mesh.setNodeID(nodeID);
- 
+
   // Set the PA Level to MIN and disable LNA for testing & power supply related issues
   radio.begin();
   radio.setPALevel(RF24_PA_MIN, 0);
- 
+
   // Connect to the mesh
   Serial.println(F("Connecting to the mesh..."));
-  if (!mesh.begin()) {
-    if (radio.isChipConnected()) {
-      do {
+  if (!mesh.begin())
+  {
+    if (radio.isChipConnected())
+    {
+      do
+      {
         // mesh.renewAddress() will return MESH_DEFAULT_ADDRESS on failure to connect
         Serial.println(F("Could not connect to network.\nConnecting to the mesh..."));
       } while (mesh.renewAddress() == MESH_DEFAULT_ADDRESS);
-    } else {
+    }
+    else
+    {
       Serial.println(F("Radio hardware not responding."));
-      while (1) {
+      while (1)
+      {
         // hold in an infinite loop
       }
     }
   }
 }
- 
- 
- 
-void loop() {
- 
+
+void loop()
+{
+
   mesh.update();
- 
+
   // Send to the master node every second
-  if (millis() - displayTimer >= 1000) {
+  if (millis() - displayTimer >= 1000)
+  {
     displayTimer = millis();
- 
+
     // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&displayTimer, 'M', sizeof(displayTimer))) {
- 
+    if (!mesh.write(&displayTimer, 'M', sizeof(displayTimer)))
+    {
+
       // If a write fails, check connectivity to the mesh network
-      if (!mesh.checkConnection()) {
-        //refresh the network address
+      if (!mesh.checkConnection())
+      {
+        // refresh the network address
         Serial.println("Renewing Address");
-        if (mesh.renewAddress() == MESH_DEFAULT_ADDRESS) {
-          //If address renewal fails, reconfigure the radio and restart the mesh
-          //This allows recovery from most if not all radio errors
+        if (mesh.renewAddress() == MESH_DEFAULT_ADDRESS)
+        {
+          // If address renewal fails, reconfigure the radio and restart the mesh
+          // This allows recovery from most if not all radio errors
           mesh.begin();
         }
-      } else {
+      }
+      else
+      {
         Serial.println("Send fail, Test OK");
       }
-    } else {
+    }
+    else
+    {
       Serial.print("Send OK: ");
       Serial.println(displayTimer);
     }
   }
- 
-  while (network.available()) {
+
+  while (network.available())
+  {
     RF24NetworkHeader header;
     payload_t payload;
     network.read(header, &payload, sizeof(payload));
