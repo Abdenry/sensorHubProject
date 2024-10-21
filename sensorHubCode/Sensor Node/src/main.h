4528d2fb54214febd1818a41bb97442c990ff4d3
Hiro <81205061+Abdenry@users.noreply.github.com>
2024-10-21 15:45:15 +1000

    Include and Define

---
+++ sensorHubCode/Sensor Node/src/main.h
@@ -0,0 +1,11 @@
+#include <avr/io.h>
+#include <RF24.h>
+#include <RF24Network.h>
+#include <RF24Mesh.h>
+#include <SPI.h>
+#include <DFRobot_AHT20.h>
+#include <LPS.h>
+
+#define CE 9
+#define CS 10
+#define nodeID 4
