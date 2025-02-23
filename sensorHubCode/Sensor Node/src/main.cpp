2a32a624e381bb8fd8359bb21f7c593339d4b06b
Hiro <81205061+Abdenry@users.noreply.github.com>
2025-02-23 17:19:38 +1000

    Code Refactor; Removed Blocking Time Interval
    - Dedicated setup functions for different parts of the system.
    
    - Software timer now used to send a packet X seconds rather than a blocking time difference check. Done to mitigate chances of blocking code interfering with interrupts.

--- sensorHubCode/Sensor Node/src/main.cpp
+++ sensorHubCode/Sensor Node/src/main.cpp
@@ -1,56 +1,107 @@
 #include <main.h>
-#define sendPackIntervalSec 5
+#define sendPackIntervalSec 300
+volatile int secondsPast = 0;
 
-bool initAHT20 = true;
-bool initLSP = true;
-
-/**** Configure the nrf24l01 CE and CS pins ****/
+/**** Radio ****/
 RF24 radio(CE, CS);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
-uint32_t displayTimer = 0;
 
-/**** Configure the AHT20****/
+/**** AHT20 ****/
 DFRobot_AHT20 aht20;
 uint8_t status;
+bool initAHT20 = true;
 
-/**** Configure the LPS331AP****/
-
+/**** LPS331AP ****/
 LPS lps331ap;
+bool initLSP = true;
 
-/**** Configure the ANEMOMETER****/
-
+/**** ANEMOMETER ****/
 volatile int revolutionsAnemometerCount;
 
 sensorData_t packet = {0,0,0,0,1,1};
 
 void setup()
 {
-  /**** Configure the ANEMOMETER****/
-  DDRD &= ~(1 << 2);
-  PORTD |= (1 << 2);
-
-  EICRA |= (1 << ISC01);
-  EIMSK |= (1 << INT0);
-
+  anemometerSetup();
+  softwareTimerSetup();
   Serial.begin(115200);
   while (!Serial){}
+  aht20Setup();
+  lps331apSetup();
+  radioSetup();
+  Serial.println(F("Connected!"));
+  sei();
+}
 
+void loop()
+{
+  mesh.update();
+
+  if (secondsPast >= sendPackIntervalSec)
+  {
+    getWindspeed(&packet, anemometerArmDistMetres, conversionRatio, &revolutionsAnemometerCount);
+
+    if(packet.initAHT20){
+      aht20.reset();
+      getAHT20Data(&packet, aht20);
+    }
+    
+    if(packet.initLSP){
+      getlsp331AP(&packet, lps331ap);
+    }
+
+    sendPacket(&packet, 77, sizeof(packet));
+    secondsPast = 0;
+  }
+}
+
+/**** AUXILIARY FUNCTIONS ****/
+
+void lps331apSetup()
+{
+  if (!lps331ap.init())
+  {
+    Serial.println("Failed to autodetect pressure sensor!");
+    packet.initLSP = 0;
+  }
+  else
+  {
+    lps331ap.enableDefault();
+  }
+}
+
+void aht20Setup()
+{
   if ((status = aht20.begin()) != 0)
   {
     Serial.print("AHT20 Sensor init failed. error status: ");
     Serial.println(status);
     packet.initAHT20 = 0;
   }
+}
 
-  if (!lps331ap.init())
-  {
-    Serial.println("Failed to autodetect pressure sensor!");
-    packet.initLSP = 0;
-  }else{
-    lps331ap.enableDefault();
-  }
+void softwareTimerSetup()
+{
+  /**** Configure the TIMER1****/
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1B |= (1 << WGM12) | (1 << CS12);
+  OCR1A = (F_CPU / 256) - 1;
+  TIMSK1 |= (1 << OCIE1A);
+}
 
+void anemometerSetup()
+{
+  /**** Configure the ANEMOMETER****/
+  DDRD &= ~(1 << 2);
+  PORTD |= (1 << 2);
+  EICRA |= (1 << ISC01);
+  EIMSK |= (1 << INT0);
+}
+
+void radioSetup()
+{
   mesh.setNodeID(nodeID);
   radio.begin();
   radio.setPALevel(RF24_PA_MIN, 0);
@@ -73,33 +124,6 @@ void setup()
       {
       }
     }
-  }
-  
-  Serial.println(F("Connected!"));
-  sei();
-}
-
-void loop()
-{
-  mesh.update();
-
-  // Send to the master node every x seconds
-  if (millis() - displayTimer >= (sendPackIntervalSec * 1000))
-  {
-    displayTimer = millis();
-
-    getWindspeed(&packet, anemometerArmDistMetres, conversionRatio, &revolutionsAnemometerCount);
-    
-    if(packet.initAHT20){
-      aht20.reset();
-      getAHT20Data(&packet, aht20);
-    }
-
-    if(packet.initLSP){
-      getlsp331AP(&packet, lps331ap);
-    }
-
-    sendPacket(&packet, 77, sizeof(packet));
   }
 }
 
@@ -152,4 +176,8 @@ void getlsp331AP(sensorData_t *packet, LPS lps331ap){
 ISR(INT0_vect)
 {
   revolutionsAnemometerCount += 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+  secondsPast += 1;
 }
--- sensorHubCode/Sensor Node/src/main.h
+++ sensorHubCode/Sensor Node/src/main.h
@@ -6,23 +6,15 @@
 #include <SPI.h>
 #include <DFRobot_AHT20.h>
 #include <LPS.h>
+#include "Arduino.h"
 
+#define F_CPU 16000000UL
 #define CE 9
 #define CS 10
 #define nodeID 4
 
 const float anemometerArmDistMetres = 0.1;
 const float conversionRatio = (2 * PI) / 60;
-
-
-// typedef struct sensorData_t {
-//     bool initAHT20;
-//     float aht20tempC;
-//     float aht20RH;
-//     bool initLSP;
-//     float lspBar;
-//     float windSpeed;
-// } sensorData_t;
 
 typedef struct sensorData_t
 {
@@ -34,10 +26,23 @@ typedef struct sensorData_t
   char initLSP;
 }sensorData_t;
 
+void lps331apSetup();
+
+void aht20Setup();
+
+void softwareTimerSetup();
+
+void anemometerSetup();
+
+void radioSetup();
+
 void sendPacket(sensorData_t *packet, uint8_t msg_type, size_t packetSize);
 
 void getWindspeed(sensorData_t *packet, float anemometerArmDistMetres, float conversionRatio, volatile int* revolutionsAnemometerCount);
 
+void getWindDirection(sensorData_t *packet);
+
 void getAHT20Data(sensorData_t *packet, DFRobot_AHT20 aht20);
 
 void getlsp331AP(sensorData_t *packet, LPS lps331ap);
+
